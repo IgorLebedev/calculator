350089d482f079eb2f7e7e66a2be8fa9f90ee9e0
igorlebedev <igorlebedev228@gmail.com>
2023-07-13 17:40:44 +0700

    some layout changes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Input = () => {
   return (
-    <div className="containter text-4xl">
-      <input className="bg-orange-500 text-white h-20 px-5 pb-5 pt-5 outline-none w-full text-right" type="text" />
+    <div className="relative grow text-6xl bg-slate-950">
+      <input className="absolute bottom-0 bg-transparent text-white w-full h-auto pb-0 pt-10 px-8 outline-none text-right" type="text" />
     </div>
   );
 };
