6116ca40fbf98f9c79ae3d9e3aaf6fd93904ca2e
igorlebedev <igorlebedev228@gmail.com>
2023-07-13 17:41:19 +0700

    minor fixes

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { buttons } from "./buttons";
 import Button from "./button";
 
-const ButtonsContainer = () => {
+const ButtonsContainer = (): ReactElement => {
   return (
-    <div className="container grid grid-cols-12 gap-1 px-4 py-4">
+    <div className="container grid grid-cols-12 gap-3.5 px-4 py-4">
       {buttons.map((value) => <Button value={value} />)}
     </div>
   );
