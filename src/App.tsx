29334dcaccec4ace0c3c32d2b066fce400e46e57
igorlebedev <igorlebedev228@gmail.com>
2023-07-13 13:07:41 +0700

    height fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ButtonsContainer from "./components/buttons";
 function App() {
   return (
     <div className="m-0 flex justify-center items-center bg-neutral-950 w-screen h-screen">
-      <div className="container w-screen h-screen bg-slate-600 sm:h-[460px] sm:w-96">
+      <div className="container w-screen h-auto bg-slate-600 sm:h-auto sm:w-96">
         <Input />
         <ButtonsContainer />
       </div>
