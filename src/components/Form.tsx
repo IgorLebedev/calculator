59dfb235d76398a5a7e206e08875a63b0d64308e
igorlebedev <igorlebedev228@gmail.com>
2023-07-13 13:07:49 +0700

    input changes

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Input = () => {
   return (
-    <div className="containter text-3xl">
-      <input className="bg-orange-500 px-5 pb-3 pt-10 outline-none w-full" type="text" />
+    <div className="containter text-4xl">
+      <input className="bg-orange-500 text-white h-20 px-5 pb-5 pt-5 outline-none w-full text-right" type="text" />
     </div>
   );
 };
