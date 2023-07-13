7b2a103334f19cf3f464d168f4a44c7c2ffdabea
igorlebedev <igorlebedev228@gmail.com>
2023-07-13 17:41:06 +0700

    change some buttons symbols

--- src/components/buttons/buttons.ts
+++ src/components/buttons/buttons.ts
@@ -1,3 +1,3 @@
-export const buttons = ['C', '+/-', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', ',', '='];
-export const lightGrayBtns = ['C', '+/-', '%'];
-export const orangeBtns = ['/', '*', '-', '+', '='];
+export const buttons = ['C', '±', '%', '÷', '7', '8', '9', '✖', '4', '5', '6', '-', '1', '2', '3', '+', '0', ',', '='];
+export const lightGrayBtns = ['C', '±', '%'];
+export const orangeBtns = ['÷', '✖', '-', '+', '='];
