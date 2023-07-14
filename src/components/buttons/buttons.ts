621264d92203182f40c9b11cdc4294efaf04b3ed
igorlebedev <igorlebedev228@gmail.com>
2023-07-14 14:52:56 +0700

    remove

--- src/components/buttons/buttons.ts
+++
@@ -1,3 +0,0 @@
-export const buttons = ['C', '±', '%', '÷', '7', '8', '9', '✖', '4', '5', '6', '-', '1', '2', '3', '+', '0', ',', '='];
-export const lightGrayBtns = ['C', '±', '%'];
-export const orangeBtns = ['÷', '✖', '-', '+', '='];
