f84e465ec9726ba5fc0899e571a65f83a5532b75
igorlebedev <igorlebedev228@gmail.com>
2023-07-14 14:53:05 +0700

    rework buttons container

---
+++ src/components/buttons/ButtonsContainer.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from "react";
+import Operands from "./Operands";
+import Operators from "./Operators";
+import Utilites from "./Utilities";
+
+const ButtonsContainer = ({ setInput }: { setInput: React.Dispatch<React.SetStateAction<string>> }): ReactElement => {
+  return (
+    <div className="container grid grid-cols-12 gap-3.5 px-4 py-4">
+      <Utilites />
+      <Operators />
+      <Operands />
+    </div>
+  );
+};
+
+export default ButtonsContainer;
