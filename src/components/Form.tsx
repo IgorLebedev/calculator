1289ea122f726df05fc7af8bc3149617b376774f
igorlebedev <igorlebedev228@gmail.com>
2023-07-14 14:52:49 +0700

    rework

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Input = () => {
+const Input = ({ input }: { input: string }) => {
   return (
-    <div className="relative grow text-6xl bg-slate-950">
-      <input className="absolute bottom-0 bg-transparent text-white w-full h-auto pb-0 pt-10 px-8 outline-none text-right" type="text" />
+    <div className="relative grow text-7xl bg-slate-950">
+      <input
+        className="absolute bottom-0 bg-transparent text-white w-full h-auto pb-0 pt-10 px-8 outline-none text-right"
+        type="text"
+        value={input}
+        disabled
+      />
     </div>
   );
 };
