c6ffd11457a498acfdab0a3a110f0ad14735a4b3
igorlebedev <igorlebedev228@gmail.com>
2023-07-17 16:19:39 +0700

    black bg

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import ButtonsContainer from "./components/buttons/ButtonsContainer";
 function App() {
   return (
     <div className="m-0 flex justify-center items-center bg-neutral-950 w-screen h-screen">
-      <div className="flex flex-col w-screen h-full bg-slate-950 sm:w-96 font-san-francisco">
+      <div className="flex flex-col w-screen h-full bg-black sm:w-96 font-san-francisco">
         <Input />
         <ButtonsContainer/>
       </div>
