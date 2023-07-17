1df79b0cdbdac847a350fb9ac6dc621bcdfbd392
igorlebedev <igorlebedev228@gmail.com>
2023-07-17 17:13:53 +0700

    change color

--- src/components/buttons/Utilities.tsx
+++ src/components/buttons/Utilities.tsx
@@ -4,7 +4,7 @@ import useStore from "../../store/store";
 const Utilites = () => {
   const remove = useStore((store) => store.remove);
   const isFirstZero = useStore((store) => store.isFirstZero);
-  const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 text-black text-4xl hover:bg-opacity-40 hover:transition-none transition duration-1000 active:translate-x-0.5 active:translate-y-0.5'
+  const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 text-black text-4xl hover:bg-grey-lightHover hover:transition-none transition duration-1000 active:translate-x-0.5 active:translate-y-0.5'
   return (
     <div className="grid grid-cols-3 gap-3.5 col-span-9 row-span-1">
       <button
@@ -21,7 +21,7 @@ const Utilites = () => {
       <button
         className={`${classNames}`}
       >
-        %
+        {'\u0025'}
       </button>
     </div>
   )
