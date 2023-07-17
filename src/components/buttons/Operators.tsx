7540832f54cec833701143151a86af99c7e6bcd6
igorlebedev <igorlebedev228@gmail.com>
2023-07-17 17:13:36 +0700

    change colors

--- src/components/buttons/Operators.tsx
+++ src/components/buttons/Operators.tsx
@@ -1,31 +1,38 @@
 import React from "react";
+import useStore from "../../store/store";
 
 const Operators = () => {
-  const classNames  = 'w-20 h-20 rounded-full bg-mandarin col-span-1 text-white text-4xl hover:bg-opacity-40 hover:transition-none transition duration-1000 active:translate-x-0.5 active:translate-y-0.5'
+  const setOperator = useStore((store) => store.setOperator);
+  const operator = useStore((store) => store.operator);
+  const classNames  = 'w-20 h-20 rounded-full col-span-1  text-4xl hover:bg-mandarin-hover hover:transition-none transition duration-1000 active:translate-x-0.5 active:translate-y-0.5'
   return (
     <div className="grid grid-cols-1 gap-3.5 col-span-3 row-span-5">
       <button
-        className={`${classNames}`}
+        className={`${classNames} ${operator === '/' ? 'text-mandarin-common bg-white' : 'text-white bg-mandarin-common'}`}
+        onClick={() => setOperator('/')}
       >
         ÷
       </button>
       <button
-        className={`${classNames}`}
+        className={`${classNames} ${operator === '*' ? 'text-mandarin-common bg-white' : 'text-white bg-mandarin-common'}`}
+        onClick={() => setOperator('*')}
       >
-        ✖
+        {'\u00D7'}
       </button>
       <button
-        className={`${classNames}`}
+        className={`${classNames} ${operator === '-' ? 'text-mandarin-common bg-white' : 'text-white bg-mandarin-common'}`}
+        onClick={() => setOperator('-')}
       >
         -
       </button>
       <button
-        className={`${classNames}`}
+        className={`${classNames} ${operator === '+' ? 'text-mandarin-common bg-white' : 'text-white bg-mandarin-common'}`}
+        onClick={() => setOperator('+')}
       >
         +
       </button>
       <button
-        className={`${classNames}`}
+        className={`${classNames} text-white bg-mandarin-common`}
       >
        =
       </button>
