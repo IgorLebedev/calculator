e9d83f49a097b5ba93b4391f81c9b2d349b3b095
igorlebedev <igorlebedev228@gmail.com>
2023-07-17 15:00:47 +0700

    add input logic

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import useStore from "../store/store";
 
-const Input = ({ input }: { input: string }) => {
+const Input = () => {
+  const input = useStore((store) => {
+    if (store.operator !== null) {
+      return store.secondOperand.join('');
+    }
+    return store.firstOperand.join('');
+  });
+  console.log(input)
   return (
     <div className="relative grow text-7xl bg-slate-950">
       <input
