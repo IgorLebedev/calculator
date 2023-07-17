5a3d610934e59f416dc634082bbfc2c2a8318275
igorlebedev <igorlebedev228@gmail.com>
2023-07-17 15:00:33 +0700

    remove useless logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Input from "./components/Form";
 import ButtonsContainer from "./components/buttons/ButtonsContainer";
 
 function App() {
-  const [input, setInput] = useState('0');
   return (
     <div className="m-0 flex justify-center items-center bg-neutral-950 w-screen h-screen">
       <div className="flex flex-col w-screen h-full bg-slate-950 sm:w-96 font-san-francisco">
-        <Input input={input}/>
-        <ButtonsContainer setInput={setInput}/>
+        <Input />
+        <ButtonsContainer/>
       </div>
     </div>
   );
