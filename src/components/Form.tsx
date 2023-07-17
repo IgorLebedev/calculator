1fac9de9256ddc2950db557b63b7f703991efe93
igorlebedev <igorlebedev228@gmail.com>
2023-07-17 17:13:01 +0700

    minor rework

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,14 +3,14 @@ import useStore from "../store/store";
 
 const Input = () => {
   const input = useStore((store) => {
-    if (store.operator !== null) {
-      return store.secondOperand.join('');
+    if (store.operator !== null && !store.isFirstZero) {
+      return store.secondOperand;
     }
-    return store.firstOperand.join('');
+    return store.firstOperand;
   });
   console.log(input)
   return (
-    <div className="relative grow text-7xl bg-slate-950">
+    <div className="relative grow text-7xl bg-transparent">
       <input
         className="absolute bottom-0 bg-transparent text-white w-full h-auto pb-0 pt-10 px-8 outline-none text-right"
         type="text"
