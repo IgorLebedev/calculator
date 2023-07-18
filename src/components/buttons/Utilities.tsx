ccea0eb11c5f2ad26cb9d4dc75abeb5bf6757b8f
igorlebedev <igorlebedev228@gmail.com>
2023-07-18 15:38:46 +0700

    minor fix

--- src/components/buttons/Utilities.tsx
+++ src/components/buttons/Utilities.tsx
@@ -3,15 +3,15 @@ import useStore from "../../store/store";
 
 const Utilites = () => {
   const remove = useStore((store) => store.remove);
-  const isFirstZero = useStore((store) => store.isFirstZero);
-  const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 text-black text-4xl hover:bg-grey-lightHover hover:transition-none transition duration-1000 active:translate-x-0.5 active:translate-y-0.5'
+  const firstOperand = useStore((store) => store.firstOperand);
+  const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 text-black text-4xl hover:bg-grey-lightHover btn-anim'
   return (
     <div className="grid grid-cols-3 gap-3.5 col-span-9 row-span-1">
       <button
         className={`${classNames}`}
         onClick={remove}
       >
-        {isFirstZero ? 'AC' : 'C'}
+        {firstOperand === '0' ? 'AC' : 'C'}
       </button>
       <button
         className={`${classNames}`}
