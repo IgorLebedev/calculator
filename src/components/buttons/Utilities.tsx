a59d851205903589bfb84d42b7646e9e3af73ed3
igorlebedev <igorlebedev228@gmail.com>
2023-07-19 16:25:36 +0700

    add minus onclick

--- src/components/buttons/Utilities.tsx
+++ src/components/buttons/Utilities.tsx
@@ -3,18 +3,22 @@ import useStore from "../../store/store";
 
 const Utilites = () => {
   const remove = useStore((store) => store.remove);
+  const minus = useStore((store) => store.minus);
   const firstOperand = useStore((store) => store.firstOperand);
-  const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 text-black text-4xl hover:bg-grey-lightHover btn-anim'
+  const operator = useStore((store) => store.operator);
+  const isAC = (firstOperand === '0' || firstOperand === '−') && !operator
+  const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 font-medium text-3xl hover:bg-grey-lightHover btn-anim'
   return (
     <div className="grid grid-cols-3 gap-3.5 col-span-9 row-span-1">
       <button
         className={`${classNames}`}
         onClick={remove}
       >
-        {firstOperand === '0' ? 'AC' : 'C'}
+        {isAC ? 'AC' : 'C'}
       </button>
       <button
         className={`${classNames}`}
+        onClick={minus}
       >
         ±
       </button>
