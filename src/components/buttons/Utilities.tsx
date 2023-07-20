480db452b1afa8167b656f02994ab1d07fad83f0
igorlebedev <igorlebedev228@gmail.com>
2023-07-20 17:53:39 +0700

    add percent onclick

--- src/components/buttons/Utilities.tsx
+++ src/components/buttons/Utilities.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import useStore from "../../store/store";
 
 const Utilites = () => {
-  const remove = useStore((store) => store.remove);
-  const minus = useStore((store) => store.minus);
-  const firstOperand = useStore((store) => store.firstOperand);
-  const operator = useStore((store) => store.operator);
-  const isAC = (firstOperand === '0' || firstOperand === '−') && !operator
+  const remove = useStore(store => store.remove);
+  const minus = useStore(store => store.minus);
+  const percent = useStore(store => store.percent);
+  const firstOperand = useStore(store => store.firstOperand);
+  const operator = useStore(store => store.operator);
+  const isAC = (firstOperand === '0' || firstOperand === '−') && !operator;
   const classNames  = 'w-20 h-20 rounded-full bg-grey-light col-span-1 font-medium text-3xl hover:bg-grey-lightHover btn-anim'
   return (
     <div className="grid grid-cols-3 gap-3.5 col-span-9 row-span-1">
@@ -24,6 +25,7 @@ const Utilites = () => {
       </button>
       <button
         className={`${classNames}`}
+        onClick={percent}
       >
         {'\u0025'}
       </button>
