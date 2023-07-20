daeb8e092be802b7eda85eda12deac89218131e8
igorlebedev <igorlebedev228@gmail.com>
2023-07-20 16:01:10 +0700

    adding spaces

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import useStore from "../store/store";
+import addSpaces from "../utils/addSpaces";
 
 const Input = () => {
-  const input = useStore((store) => {
+  const rawInput = useStore((store) => {
     if (store.operator !== null && !store.isFirstZero) {
       return store.secondOperand;
     }
     return store.firstOperand;
   });
-  const fontSizing = input.length > 6 ? 'text-6xl' : 'text-7xl';
+  const fixedInput = addSpaces(rawInput);
+  const fontSizing = rawInput.length > 6 ? 'text-6xl' : 'text-7xl';
   return (
     <div className={`relative grow font-thin bg-transparent ${fontSizing}`}>
       <input
         className="absolute bottom-0 bg-transparent text-white w-full h-auto pb-0 pt-10 px-8 outline-none text-right"
         type="text"
-        value={input}
+        value={fixedInput}
         disabled
       />
     </div>
